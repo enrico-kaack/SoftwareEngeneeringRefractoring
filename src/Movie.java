5fcd2ad94611f672eb95090d147d6d3cf3b3d309
enrico-kaack <e.kaack@live.de>
2017-05-10 14:44:37 +0200

    Added Price state class concept

--- src/Movie.java
+++ src/Movie.java
@@ -1,22 +1,41 @@
 public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-    private String title;
-    private int priceCode;
-    public Movie(String newtitle, int newpriceCode) {
-        title = newtitle;
-        priceCode = newpriceCode;
-    }
-    public int getPriceCode() {
-        return priceCode;
-    }
-    public void setPriceCode(int arg) {
-        priceCode = arg;
-    }
-    public String getTitle (){
-        return title;
-    }
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+	private String title;
+
+	public Movie(String newtitle, int newpriceCode) {
+		title = newtitle;
+		setPriceCode(newpriceCode);
+		;
+	}
+
+	public int getPriceCode() {
+		return _price.getPriceCode();
+	}
+
+	public void setPriceCode(int arg) {
+		switch (arg) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrensPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
+	}
+
+	private Price _price;
+
+	public String getTitle() {
+		return title;
+	}
+
 	double getCharge(int daysRent) {
 		double result = 0;
 		switch (getPriceCode()) {
@@ -36,6 +55,7 @@ public class Movie {
 		}
 		return result;
 	}
+
 	int getFrequentRenterPoints(int daysRent) {
 		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRent > 1)
 			return 2;
