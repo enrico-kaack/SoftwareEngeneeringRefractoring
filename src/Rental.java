9a005b5c2c39edf775bc5b9c873e432cd5525570
enrico-kaack <e.kaack@live.de>
2017-05-10 14:08:42 +0200

    Extract and moved getFrequentRenterPoints()

--- src/Rental.java
+++ src/Rental.java
@@ -30,4 +30,12 @@ class Rental {
 	    }
 	    return result;
 	}
+	int getFrequentRenterPoints(int frequentRenterPoints) {
+		// add frequent renter points
+		frequentRenterPoints ++;
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+		    frequentRenterPoints ++;
+		return frequentRenterPoints;
+	}
 }
